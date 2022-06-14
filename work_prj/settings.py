93d77d834c16aad540a0dc052d0a760cf9b29285
viveknhc <viveknhc@gmail.com>
2022-06-14 23:11:27 +0530

    ver1

--- work_prj/settings.py
+++ work_prj/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workapp'
+    'workapp',
+    'rest_framework',
+    'exam',
+    'examuser'
 ]
 
 MIDDLEWARE = [
@@ -77,10 +80,18 @@ WSGI_APPLICATION = 'work_prj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'ecom',
+        'USER':'postgres',
+        'PASSWORD':'vivek123',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -124,8 +135,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIR=[
     os.path.join(BASE_DIR,'work_app/static')
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
